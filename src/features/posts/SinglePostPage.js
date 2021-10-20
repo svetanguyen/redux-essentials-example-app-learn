eb2819b0565467b5e37671071c3581ef3fcc99a3
svetanguyen <matematicaforeber@gmail.com>
2021-10-20 10:11:37 +0700

    add fake api to render posts and add posts

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -3,13 +3,13 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from "./ReactionButtons";
+import { TimeAgo } from "./TimeAgo";
+import { selectPostById } from './postsSlice'
 
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params
 
-    const post = useSelector(state => 
-        state.posts.find(post => post.id === postId)
-        )
+    const post = useSelector(state => selectPostById(state, postId))
 
     if (!post) {
       return (
@@ -23,12 +23,13 @@ export const SinglePostPage = ({ match }) => {
       <section>
         <article className="post">
           <h2>{post.title}</h2>
+          <PostAuthor userId={post.id} />
+          <TimeAgo timestamp={post.date} />
           <p className="post-content">{post.content}</p>
-					<PostAuthor userId={post.id} />
+          <ReactionButtons post={post} />
 					<Link to={`/editPost/${post.id}`} className="button">
 						Edit Post
 					</Link>
-          <ReactionButtons post={post} />
         </article>
       </section>
     )
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice'
 
 import { worker } from './api/server'
 
 // Start our mock API server
 worker.start({ onUnhandledRequest: 'bypass' })
-
+store.dispatch(fetchUsers())
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
