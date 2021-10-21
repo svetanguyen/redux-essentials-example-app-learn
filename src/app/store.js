46a3b08a3d1e0c565100de33f1be8caf1aecd4e0
svetanguyen <matematicaforeber@gmail.com>
2021-10-21 10:29:54 +0700

    add notifications

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
 import usersSlice from '../features/users/usersSlice'
+import notificationsReducer  from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
-    users: usersSlice
+    users: usersSlice,
+    notifications: notificationsReducer
   },
 })
